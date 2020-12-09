915058928b3507a4d1bf152911a0a9e43ccda0ef
abd <abdelgzali@gmail.com>
2020-12-08 23:46:10 -0800

    build

---
+++ client/build/precache-manifest.7c9a7fd55ae6e543e0136e8686017d40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440e06dc7fc2059a951037d48b7b3d90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dc632a1f83d34358d14",
+    "url": "/static/css/main.2d1f4915.chunk.css"
+  },
+  {
+    "revision": "3337b5296f1a9043a7a5",
+    "url": "/static/js/2.c0e66ea1.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/static/js/2.c0e66ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dc632a1f83d34358d14",
+    "url": "/static/js/main.b4fd8c8a.chunk.js"
+  },
+  {
+    "revision": "4e6e91f0606716916ff5",
+    "url": "/static/js/runtime-main.c8540731.js"
+  }
+]);
